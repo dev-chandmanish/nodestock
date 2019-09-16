fe97e9e58aa2af22c052a4fac011fe8edbb93ee2
Manish Chand <chandmanish99@gmail.com>
2019-09-17 00:45:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//set handlebars routes
+app.get('/', function(req, res){
+	res.render('home', {
+		stuff:"This is stuff"
+	});
+})
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server Listening on port '+ PORT))
